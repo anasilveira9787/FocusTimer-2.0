92f46318ef7b3a982582c933655ea67a8c53c91d
Ana Silveira <ana.ccord@gmail.com>
2023-04-18 15:08:11 -0300

    fix: fixed file paths

--- scripts/sounds.js
+++ scripts/sounds.js
@@ -1,9 +1,9 @@
 export default function() {
 
-    const forestSound = new Audio("../src/Floresta.wav")
-    const rainSound = new Audio("../src/Chuva.wav")
-    const coffeshopSound = new Audio("../src/Cafeteria.wav")
-    const fireplaceSound = new Audio("../src/Lareira.wav")
+    const forestSound = new Audio("src/Floresta.wav")
+    const rainSound = new Audio("src/Chuva.wav")
+    const coffeshopSound = new Audio("src/Cafeteria.wav")
+    const fireplaceSound = new Audio("src/Lareira.wav")
 
     const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
     const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
